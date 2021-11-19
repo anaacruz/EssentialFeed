a8355f66a1c390ece77f34dfc004be047f2b8a23
Ana Laura Anguiano Cruz <ana.anguiano@wizeline.com>
2021-11-19 14:05:55 -0600

    Hide cache expiration details from tests with a new DSL method to protect tests from breaking changes

--- EssentialFeed/Feed Cache/FeedCachePolicy.swift
+++ EssentialFeed/Feed Cache/FeedCachePolicy.swift
@@ -7,17 +7,16 @@
 
 import Foundation
 
-private final class FeedCachePolicy {
-    
+internal final class FeedCachePolicy {
     private init() {}
-    
+
     private static let calendar = Calendar(identifier: .gregorian)
-    
+
     private static var maxCacheAgeInDays: Int {
         return 7
     }
-    
-    static func validate(_ timestamp: Date, against date: Date) -> Bool {
+
+    internal static func validate(_ timestamp: Date, against date: Date) -> Bool {
         guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else {
             return false
         }

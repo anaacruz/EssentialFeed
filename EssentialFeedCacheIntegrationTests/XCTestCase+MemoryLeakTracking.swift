1a7e57eec2c3198a75109b237a3ebdc4837897fb
Ana Laura Anguiano Cruz <ana.anguiano@wizeline.com>
2021-11-23 18:55:02 -0600

    Include memory leak tracking helper in the cache integration tests target

---
+++ EssentialFeedCacheIntegrationTests/XCTestCase+MemoryLeakTracking.swift
@@ -0,0 +1,14 @@
+//
+//  XCTestCase+MemoryLeakTracking.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Ana Anguiano Cruz on 23/11/21.
+//
+
+import XCTest
+
+class XCTestCase_MemoryLeakTracking: XCTestCase {
+
+
+
+}

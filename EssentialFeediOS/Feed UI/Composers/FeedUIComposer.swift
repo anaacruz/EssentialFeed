da7a1753b07ae339682a44e08d02c04c2eb48ddb
Ana Laura Anguiano Cruz <ana.anguiano@wizeline.com>
2021-12-01 01:54:29 -0600

    Unify FeedViewController static factory helper into the `FeedUIComposer`

--- EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -6,30 +6,27 @@
 //
 import UIKit
 import EssentialFeed
-
 public final class FeedUIComposer {
     private init() {}
     
     public static func feedComposedWith(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
         let presentationAdapter = FeedLoaderPresentationAdapter(feedLoader:
-                                                                    MainQueueDispatchDecorator(decoratee: feedLoader))
-        
-        let feedController = FeedViewController.makeWith(
+            MainQueueDispatchDecorator(decoratee: feedLoader))
+
+        let feedController = makeFeedViewController(
             delegate: presentationAdapter,
             title: FeedPresenter.title)
-        
+
         presentationAdapter.presenter = FeedPresenter(
             feedView: FeedViewAdapter(
-                            controller: feedController,
-                            imageLoader: MainQueueDispatchDecorator(decoratee: imageLoader)),
+                controller: feedController,
+                imageLoader: MainQueueDispatchDecorator(decoratee: imageLoader)),
             loadingView: WeakRefVirtualProxy(feedController))
+
         return feedController
     }
-    
-}
 
-private extension FeedViewController {
-    static func makeWith(delegate: FeedViewControllerDelegate, title: String) -> FeedViewController {
+    private static func makeFeedViewController(delegate: FeedViewControllerDelegate, title: String) -> FeedViewController {
         let bundle = Bundle(for: FeedViewController.self)
         let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
         let feedController = storyboard.instantiateInitialViewController() as! FeedViewController
